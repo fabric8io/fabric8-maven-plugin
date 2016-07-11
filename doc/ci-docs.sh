9542713fe0c438a07c98bd493814b14a9326de9c
Roland Huß <roland@ro14nd.de>
2016-07-11 11:38:05 +0200

    Fix travis build

--- doc/ci-docs.sh
+++ doc/ci-docs.sh
@@ -3,8 +3,10 @@ echo Deploying fabric8-maven-plugin documentation
 echo ============================================
 
 cd doc && \
-mvn -Pdoc,html,pdf package && \
-git clone -b gh-pages git@github.com:fabric8io/fabric8-maven-plugin.git gh-pages && \
+mvn -Phtml,pdf package && \
+git clone -b gh-pages https://rhuss:${GITHUB_TOKEN}@github.com/fabric8io/fabric8-maven-plugin.git gh-pages && \
+git config --global user.email "travis@fabric8.io" && \
+git config --global user.name "Travis" && \
 cp -rv target/generated-docs/* gh-pages/ && \
 cd gh-pages && \
 mv index.pdf fabric8-maven-plugin.pdf && \
