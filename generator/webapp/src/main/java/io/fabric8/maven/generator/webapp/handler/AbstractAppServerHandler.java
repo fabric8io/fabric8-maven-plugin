09c00b0ff6340350f7bcb6bc8c32b83906b16ea5
Kamesh Sampath <kamesh.sampath@hotmail.com>
2016-10-05 16:44:34 +0530

    Fix for Issue#492

--- generator/webapp/src/main/java/io/fabric8/maven/generator/webapp/handler/AbstractAppServerHandler.java
+++ generator/webapp/src/main/java/io/fabric8/maven/generator/webapp/handler/AbstractAppServerHandler.java
@@ -9,34 +9,42 @@ import org.apache.maven.shared.utils.io.DirectoryScanner;
  */
 public abstract class AbstractAppServerHandler implements AppServerHandler {
 
+    private final String[] NO_FILES = {};
     protected MavenProject project;
 
-    protected AbstractAppServerHandler(MavenProject project){
+    protected AbstractAppServerHandler(MavenProject project) {
         this.project = project;
     }
 
     /**
      * Scan the project's output directory for certain files.
+     *
      * @param patterns one or more patterns which fit to Maven's include syntax
      * @return list of files found
      */
     protected String[] scanFiles(String... patterns) {
         String buildOutputDir =
                 project.getBuild().getOutputDirectory();
-        DirectoryScanner directoryScanner = new DirectoryScanner();
-        directoryScanner.setBasedir(buildOutputDir);
-        directoryScanner.setIncludes(patterns);
-        directoryScanner.scan();
-        return directoryScanner.getIncludedFiles();
+        if (buildOutputDir != null) {
+            DirectoryScanner directoryScanner = new DirectoryScanner();
+            directoryScanner.setBasedir(buildOutputDir);
+            directoryScanner.setIncludes(patterns);
+            directoryScanner.scan();
+            return directoryScanner.getIncludedFiles();
+        } else {
+            return NO_FILES;
+        }
+
     }
 
     /**
      * Check whether one of the given file patterns can be found
      * in the project output directory
+     *
      * @param patterns patterns to check
      * @return true if the one such file exists least
      */
-    protected boolean hasOneOf(String ... patterns) {
+    protected boolean hasOneOf(String... patterns) {
         return scanFiles(patterns).length > 0;
     }
 }
