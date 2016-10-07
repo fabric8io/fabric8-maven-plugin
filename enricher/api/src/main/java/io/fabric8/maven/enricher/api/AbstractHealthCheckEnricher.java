8291d60c3ff32b2ddc12b4d952725564cf54a2db
Nicola Ferraro <ni.ferraro@gmail.com>
2016-10-07 18:04:15 +0200

    Refactoring health check probes, moving code to karaf and spring-boot enrichers

---
+++ enricher/api/src/main/java/io/fabric8/maven/enricher/api/AbstractHealthCheckEnricher.java
@@ -0,0 +1,90 @@
+package io.fabric8.maven.enricher.api;
+
+import java.util.List;
+
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
+import io.fabric8.kubernetes.api.model.Probe;
+import io.fabric8.kubernetes.api.model.extensions.Deployment;
+
+/**
+ * Enriches containers with health check probes.
+ */
+public abstract class AbstractHealthCheckEnricher extends BaseEnricher {
+
+    public AbstractHealthCheckEnricher(EnricherContext buildContext, String name) {
+        super(buildContext, name);
+    }
+
+    @Override
+    public void addMissingResources(KubernetesListBuilder builder) {
+
+        List<HasMetadata> items = builder.getItems();
+        for (HasMetadata item : items) {
+            if (item instanceof Deployment) {
+                Deployment deployment = (Deployment) item;
+                Container container = getContainer(deployment);
+                if (container != null) {
+                    if (container.getReadinessProbe() == null) {
+                        Probe probe = getReadinessProbe();
+                        if (probe != null) {
+                            log.info("Adding readiness " + describe(probe));
+                            container.setReadinessProbe(probe);
+                        }
+                    }
+
+                    if (container.getLivenessProbe() == null) {
+                        Probe probe = getLivenessProbe();
+                        if (probe != null) {
+                            log.info("Adding liveness " + describe(probe));
+                            container.setLivenessProbe(probe);
+                        }
+                    }
+                }
+            }
+        }
+        builder.withItems(items);
+    }
+
+    private Container getContainer(Deployment deployment) {
+        if (deployment.getSpec() != null &&
+                deployment.getSpec().getTemplate() != null &&
+                deployment.getSpec().getTemplate().getSpec() != null &&
+                deployment.getSpec().getTemplate().getSpec().getContainers() != null &&
+                deployment.getSpec().getTemplate().getSpec().getContainers().size() > 0) {
+
+            List<Container> containers = deployment.getSpec().getTemplate().getSpec().getContainers();
+            return containers.get(containers.size() - 1); // last one
+        }
+
+        return null;
+    }
+
+    private String describe(Probe probe) {
+        StringBuilder desc = new StringBuilder("probe");
+        if (probe.getHttpGet() != null) {
+            desc.append(" on port ");
+            desc.append(probe.getHttpGet().getPort().getIntVal());
+            desc.append(", path='");
+            desc.append(probe.getHttpGet().getPath());
+            desc.append("'");
+        }
+        if (probe.getInitialDelaySeconds() != null) {
+            desc.append(", with initial delay ");
+            desc.append(probe.getInitialDelaySeconds());
+            desc.append(" seconds");
+        }
+        if(probe.getPeriodSeconds()!=null) {
+            desc.append(", with period ");
+            desc.append(probe.getPeriodSeconds());
+            desc.append(" seconds");
+        }
+        return desc.toString();
+    }
+
+    protected abstract Probe getReadinessProbe();
+
+    protected abstract Probe getLivenessProbe();
+
+}
