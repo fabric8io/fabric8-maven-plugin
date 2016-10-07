0ddded1c21ef67a3b3156b2726b6555f70dc9a58
Nicola Ferraro <ni.ferraro@gmail.com>
2016-10-07 08:05:31 +0200

    Adding support for reading spring-boot configuration from application.yml

--- core/src/main/java/io/fabric8/maven/core/util/MavenUtil.java
+++ core/src/main/java/io/fabric8/maven/core/util/MavenUtil.java
@@ -30,6 +30,7 @@ import org.codehaus.plexus.archiver.tar.TarLongFileMode;
 import org.codehaus.plexus.archiver.zip.ZipArchiver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -41,10 +42,13 @@ import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.SortedMap;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 
@@ -180,10 +184,17 @@ public class MavenUtil {
     }
 
     /**
-     * Returns the spring boot `application.properties` or an empty properties object if not found
+     * Returns the spring boot configuration (supports `application.properties` and `application.yml`)
+     * or an empty properties object if not found
      */
     public static Properties getSpringBootApplicationProperties(MavenProject project) {
-        return getPropertiesFile(project, "application.properties");
+        URLClassLoader compileClassLoader = getCompileClassLoader(project);
+        URL ymlResource = compileClassLoader.findResource("application.yml");
+        URL propertiesResource = compileClassLoader.findResource("application.properties");
+
+        Properties props = getPropertiesFromYamlResource(ymlResource);
+        props.putAll(getPropertiesResource(propertiesResource));
+        return props;
     }
 
     /**
@@ -192,6 +203,13 @@ public class MavenUtil {
     public static Properties getPropertiesFile(MavenProject project, String propertiesFileName) {
         URLClassLoader compileClassLoader = getCompileClassLoader(project);
         URL resource = compileClassLoader.findResource(propertiesFileName);
+        return getPropertiesResource(resource);
+    }
+
+    /**
+     * Returns the given properties resource on the project classpath if found or an empty properties object if not
+     */
+    protected static Properties getPropertiesResource(URL resource) {
         Properties answer = new Properties();
         if (resource != null) {
             try {
@@ -201,6 +219,78 @@ public class MavenUtil {
             }
         }
         return answer;
+    }
+
+    /**
+     * Returns a {@code Properties} representation of the given Yaml file on the project classpath if found or an empty properties object if not
+     */
+    public static Properties getPropertiesFromYamlFile(MavenProject project, String yamlFileName) {
+        URLClassLoader compileClassLoader = getCompileClassLoader(project);
+        URL resource = compileClassLoader.findResource(yamlFileName);
+        return getPropertiesFromYamlResource(resource);
+    }
+
+    /**
+     * Returns a {@code Properties} representation of the given Yaml resource or an empty properties object if the resource is null
+     */
+    protected static Properties getPropertiesFromYamlResource(URL resource) {
+        if (resource != null) {
+            try (InputStream yamlStream = resource.openStream()) {
+                Yaml yaml = new Yaml();
+                @SuppressWarnings("unchecked")
+                SortedMap<String, Object> source = yaml.loadAs(yamlStream, SortedMap.class);
+                Properties properties = new Properties();
+                properties.putAll(getFlattenedMap(source));
+                return properties;
+            } catch (Exception e) {
+                LOG.error("Failed to load yaml properties from: " + resource + ". " + e, e);
+            }
+        }
+        return new Properties();
+    }
+
+    /**
+     * Build a flattened representation of the Yaml tree. The conversion is compliant with the spring-boot rules.
+     */
+    private static Map<String, Object> getFlattenedMap(Map<String, Object> source) {
+        Map<String, Object> result = new LinkedHashMap<>();
+        buildFlattenedMap(result, source, null);
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void buildFlattenedMap(Map<String, Object> result, Map<String, Object> source, String path) {
+        for (Map.Entry<String, Object> entry : source.entrySet()) {
+            String key = entry.getKey();
+            if (path !=null && path.trim().length()>0) {
+                if (key.startsWith("[")) {
+                    key = path + key;
+                }
+                else {
+                    key = path + "." + key;
+                }
+            }
+            Object value = entry.getValue();
+            if (value instanceof String) {
+                result.put(key, value);
+            }
+            else if (value instanceof Map) {
+
+                Map<String, Object> map = (Map<String, Object>) value;
+                buildFlattenedMap(result, map, key);
+            }
+            else if (value instanceof Collection) {
+                Collection<Object> collection = (Collection<Object>) value;
+                int count = 0;
+                for (Object object : collection) {
+                    buildFlattenedMap(result,
+                            Collections.singletonMap("[" + (count++) + "]", object), key);
+                }
+            }
+            else {
+                result.put(key, (value != null ? value : ""));
+            }
+        }
     }
 
     /**
