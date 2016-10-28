b666f2c3dd6e9cdbc476602b2e5e2a26f983fada
James Netherton <jamesnetherton@gmail.com>
2016-10-28 11:38:36 +0100

    Improve WildFlyAppServerHandler. This fixes #502 and fixes #526.

--- generator/webapp/src/main/java/io/fabric8/maven/generator/webapp/handler/AbstractAppServerHandler.java
+++ generator/webapp/src/main/java/io/fabric8/maven/generator/webapp/handler/AbstractAppServerHandler.java
@@ -34,7 +34,7 @@ public abstract class AbstractAppServerHandler implements AppServerHandler {
      * @return list of files found
      */
     protected String[] scanFiles(String... patterns) {
-        String buildOutputDir = project.getBuild().getOutputDirectory();
+        String buildOutputDir = project.getBuild().getDirectory();
         if (buildOutputDir != null && new File(buildOutputDir).exists()) {
             DirectoryScanner directoryScanner = new DirectoryScanner();
             directoryScanner.setBasedir(buildOutputDir);
--- generator/webapp/src/test/java/io.fabric8.maven.generator.webapp/AppServerAutoDetectionTest.java
+++ generator/webapp/src/test/java/io.fabric8.maven.generator.webapp/AppServerAutoDetectionTest.java
@@ -1,15 +1,16 @@
 package io.fabric8.maven.generator.webapp;
 
-import io.fabric8.maven.generator.api.MavenGeneratorContext;
 import org.apache.maven.model.Build;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.project.MavenProject;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.IOException;
 
-import static junit.framework.TestCase.assertNotNull;
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -17,69 +18,56 @@ import static org.junit.Assert.assertEquals;
  */
 public class AppServerAutoDetectionTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void testIsWildFly() throws Exception {
-        String tmpDir = System.getProperty("java.io.tmpdir");
-        assertNotNull(tmpDir);
+        Object[] descriptorNames = new Object[] {
+            "WEB-INF/jboss-deployment-structure.xml", true,
+            "META-INF/jboss-deployment-structure.xml", true,
+            "WEB-INF/jboss-web.xml", true,
+            "WEB-INF/ejb-jar.xml", true,
+            "WEB-INF/jboss-ejb3.xml", true,
+            "META-INF/persistence.xml", true,
+            "META-INF/foo-jms.xml", true,
+            "WEB-INF/bar-jms.xml", true,
+            "META-INF/my-datasource-ds.xml", true,
+            "WEB-INF/my-datasource-ds.xml", true,
+            "WEB-INF/jboss-ejb-client.xml", true,
+            "META-INF/jbosscmp-jdbc.xml", true,
+            "WEB-INF/jboss-webservices.xml", true,
+            "META-INF/non-existent-descriptor.xml", false,
+        };
 
-        new File(tmpDir, "WEB-INF/").mkdirs();
-        new File(tmpDir, "classes/META-INF").mkdirs();
+        Object[] pluginNames = new Object[] {
+            "org.jboss.as.plugins:jboss-as-maven-plugin", true,
+            "org.wildfly.plugins:wildfly-maven-plugin", true,
+            "org.wildfly.swarm:wildfly-swarm-plugin", false,
+        };
 
-        File wildFlyDeploymentStruct = new File(tmpDir, "WEB-INF/jboss-deployment-structure.xml");
-        wildFlyDeploymentStruct.createNewFile();
-        assertNotNull(wildFlyDeploymentStruct);
-        assertTrue(wildFlyDeploymentStruct.exists());
-
-        File cdiBeansXml = new File(tmpDir, "classes/META-INF/beans.xml");
-        cdiBeansXml.createNewFile();
-        assertNotNull(cdiBeansXml);
-        assertTrue(cdiBeansXml.exists());
-
-        Model model = new Model();
-
-        Build build = new Build();
-        build.setOutputDirectory(tmpDir);
-
-        model.setBuild(build);
-
-        MavenProject mavenProject = new MavenProject(model);
-        mavenProject.setBuild(build);
-
-        AppServerHandler appServerHandler = new AppServerDetector(mavenProject).detect(null);
-        boolean actual = appServerHandler.isApplicable();
-        assertTrue(actual);
+        assertAppServerDescriptorApplicability(descriptorNames);
+        assertPluginApplicability(pluginNames);
     }
 
     @Test
     public void testIsTomcat() throws Exception {
-        String tmpDir = System.getProperty("java.io.tmpdir");
-        assertNotNull(tmpDir);
+        Object[] descriptorNames = new Object[] {
+            "META-INF/context.xml", true,
+            "META-INF/non-existent-descriptor.xml", false,
+        };
 
-        new File(tmpDir, "classes/META-INF/").mkdirs();
+        Object[] pluginNames = new Object[] {
+            "org.apache.tomcat.maven:tomcat6-maven-plugin", true,
+            "org.apache.tomcat.maven:tomcat7-maven-plugin", true,
+        };
 
-        File tomcatContextResource = new File(tmpDir, "classes/META-INF/context.xml");
-        tomcatContextResource.createNewFile();
-        assertNotNull(tomcatContextResource);
-        assertTrue(tomcatContextResource.exists());
-
-        Model model = new Model();
-
-        Build build = new Build();
-        build.setOutputDirectory(tmpDir);
-
-        model.setBuild(build);
-
-        MavenProject mavenProject = new MavenProject(model);
-        AppServerHandler appServerHandler = new AppServerDetector(mavenProject).detect(null);
-        mavenProject.setBuild(build);
-        boolean actual = appServerHandler.isApplicable();
-        assertTrue(actual);
+        assertAppServerDescriptorApplicability(descriptorNames);
+        assertPluginApplicability(pluginNames);
     }
 
     @Test
     public void testWithSpecifiedServer() throws Exception {
-
         Model model = new Model();
         MavenProject mavenProject = new MavenProject(model);
         AppServerHandler appServerHandler = new AppServerDetector(mavenProject).detect("tomcat");
@@ -89,35 +77,80 @@ public class AppServerAutoDetectionTest {
 
     @Test
     public void testIsJetty() throws Exception {
-        String tmpDir = System.getProperty("java.io.tmpdir");
-        assertNotNull(tmpDir);
+        Object[] descriptorNames = new Object[] {
+            "META-INF/jetty-logging.properties", true,
+            "WEB-INF/jetty-web.xml", true,
+            "META-INF/non-existent-descriptor.xml", false,
+        };
 
-        new File(tmpDir, "classes/META-INF/").mkdirs();
-        new File(tmpDir, "WEB-INF/").mkdirs();
+        Object[] pluginNames = new Object[] {
+            "org.mortbay.jetty:jetty-maven-plugin", true,
+            "org.eclipse.jetty:jetty-maven-plugin", true,
+        };
 
-        File jettyLoggingResource = new File(tmpDir, "classes/jetty-logging.properties");
-        jettyLoggingResource.createNewFile();
-        assertNotNull(jettyLoggingResource);
-        assertTrue(jettyLoggingResource.exists());
-
-        File jettyWebResource = new File(tmpDir, "WEB-INF/jetty-web.xml");
-        jettyWebResource.createNewFile();
-        assertNotNull(jettyWebResource);
-        assertTrue(jettyWebResource.exists());
-
-        Model model = new Model();
-
-        Build build = new Build();
-        build.setOutputDirectory(tmpDir);
-
-        model.setBuild(build);
-
-        MavenProject mavenProject = new MavenProject(model);
-        mavenProject.setBuild(build);
-        AppServerHandler appServerHandler = new AppServerDetector(mavenProject).detect(null);
-        boolean actual = appServerHandler.isApplicable();
-        assertTrue(actual);
+        assertAppServerDescriptorApplicability(descriptorNames);
+        assertPluginApplicability(pluginNames);
     }
 
+    @Test
+    public void testDefaultServer() {
+        Model model = new Model();
+        MavenProject mavenProject = new MavenProject(model);
+        AppServerHandler appServerHandler = new AppServerDetector(mavenProject).detect(null);
+        assertEquals("tomcat", appServerHandler.getName());
+    }
 
+    private void assertAppServerDescriptorApplicability(Object[] descriptors) throws IOException {
+        for (int i = 0; i < descriptors.length; i += 2) {
+            String descriptor = (String) descriptors[i];
+            boolean expected = (boolean) descriptors[i + 1];
+
+            File appDir = folder.newFolder("webapp" + i);
+            new File(appDir, "META-INF/").mkdirs();
+            new File(appDir, "WEB-INF/").mkdirs();
+            new File(appDir, descriptor).createNewFile();
+
+            Model model = new Model();
+
+            Build build = new Build();
+            build.setDirectory(appDir.getPath());
+
+            model.setBuild(build);
+
+            MavenProject mavenProject = new MavenProject(model);
+            mavenProject.setBuild(build);
+            AppServerHandler appServerHandler = new AppServerDetector(mavenProject).detect(null);
+
+            String message = String.format("Expected descriptor %s to make isApplicable() return %s", descriptor, expected);
+            assertEquals(message, expected, appServerHandler.isApplicable());
+        }
+    }
+
+    private void assertPluginApplicability(Object[] plugins) {
+        for (int i = 0; i < plugins.length; i += 2) {
+            String pluginCoordinate = (String) plugins[i];
+            String groupId = pluginCoordinate.split(":")[0];
+            String artifactId = pluginCoordinate.split(":")[1];
+            boolean expected = (boolean) plugins[i + 1];
+
+            Model model = new Model();
+
+            Build build = new Build();
+            build.setDirectory(folder.getRoot().getPath());
+
+            Plugin plugin = new Plugin();
+            plugin.setGroupId(groupId);
+            plugin.setArtifactId(artifactId);
+            build.addPlugin(plugin);
+
+            model.setBuild(build);
+
+            MavenProject mavenProject = new MavenProject(model);
+            mavenProject.setBuild(build);
+            AppServerHandler appServerHandler = new AppServerDetector(mavenProject).detect(null);
+
+            String message = String.format("Expected plugin %s to make isApplicable() return %s", pluginCoordinate, expected);
+            assertEquals(message, expected, appServerHandler.isApplicable());
+        }
+    }
 }
