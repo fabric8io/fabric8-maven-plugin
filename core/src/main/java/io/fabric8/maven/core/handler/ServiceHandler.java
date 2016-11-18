081c1943e6d15d42242f71309cd0bf764b074e99
Ioannis Canellos <iocanel@gmail.com>
2016-11-18 19:18:41 +0200

    #665: Remove break from service port iteration

--- core/src/main/java/io/fabric8/maven/core/handler/ServiceHandler.java
+++ core/src/main/java/io/fabric8/maven/core/handler/ServiceHandler.java
@@ -61,10 +61,6 @@ public class ServiceHandler {
 
             List<ServicePort> servicePorts = new ArrayList<>();
 
-            // lets default to only adding the first port as usually its the web port only
-            // TODO we could add better filters maybe?
-            // worst case folks can be specific of what ports to expose?
-            int count = 0;
             for (ServiceConfig.Port port : service.getPorts()) {
                 ServicePort servicePort = new ServicePortBuilder()
                     .withName(port.getName())
@@ -74,9 +70,6 @@ public class ServiceHandler {
                     .withNodePort(port.getNodePort())
                     .build();
                 servicePorts.add(servicePort);
-                if (++count >= 1) {
-                    break;
-                }
             }
 
             if (!servicePorts.isEmpty()) {
