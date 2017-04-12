db66aa0eb62e4a4b9043674f13c6a15eb0055dbc
Nicola Ferraro <ni.ferraro@gmail.com>
2017-04-12 12:24:17 +0200

    Fixes #832: restoring original approach for finding free port

--- core/src/test/java/io/fabric8/maven/core/util/IoUtilTest.java
+++ core/src/test/java/io/fabric8/maven/core/util/IoUtilTest.java
@@ -21,6 +21,8 @@ import java.net.ServerSocket;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class IoUtilTest {
 
     @Test
@@ -28,6 +30,17 @@ public class IoUtilTest {
         int port = IoUtil.getFreeRandomPort();
         try (ServerSocket ss = new ServerSocket(port)) {
         }
+    }
+
+    @Test
+    public void findOpenPortWhenPortsAreBusy() throws IOException {
+        int port = IoUtil.getFreeRandomPort(49152, 60000, 100);
+        try (ServerSocket ss = new ServerSocket(port)) {
+        }
+        int port2 = IoUtil.getFreeRandomPort(port, 65535, 100);
+        try (ServerSocket ss = new ServerSocket(port)) {
+        }
+        assertTrue(port2 > port);
     }
 
 }
