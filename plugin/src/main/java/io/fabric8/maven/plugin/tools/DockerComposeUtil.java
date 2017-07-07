586b75ba322ca59609afe4ba4dc60417acbc60fb
Suraj Deshmukh <surajssd009005@gmail.com>
2017-07-07 15:03:54 +0530

    code refctoring

--- plugin/src/main/java/io/fabric8/maven/plugin/tools/DockerComposeUtil.java
+++
@@ -1,7 +0,0 @@
-package io.fabric8.maven.plugin.tools;
-
-/**
- * Created by hshinde on 7/4/17.
- */
-public class ComposeUtil {
-}
--- plugin/src/test/java/io/fabric8/maven/plugin/tools/DockerComposeUtilTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.Assert.*;
-
-/**
- * Created by hshinde on 7/4/17.
- */
-public class DockerComposeUtilTest {
-
-}
