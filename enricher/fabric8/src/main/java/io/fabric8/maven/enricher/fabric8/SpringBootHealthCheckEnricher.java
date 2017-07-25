5464756ec7a83b6c2026723bbdbcb4ae69126d69
Nicola Ferraro <ni.ferraro@gmail.com>
2017-07-25 13:25:49 +0200

    Fixes #965 by improving spring-boot probes

--- enricher/fabric8/src/main/java/io/fabric8/maven/enricher/fabric8/SpringBootHealthCheckEnricher.java
+++ enricher/fabric8/src/main/java/io/fabric8/maven/enricher/fabric8/SpringBootHealthCheckEnricher.java
@@ -38,7 +38,7 @@ public class SpringBootHealthCheckEnricher extends AbstractHealthCheckEnricher {
             "org.springframework.web.context.support.GenericWebApplicationContext"
     };
 
-    private static final int DEFAULT_MANAGEMENT_PORT = 8080;
+    private static final int DEFAULT_SERVER_PORT = 8080;
     private static final String SCHEME_HTTPS = "HTTPS";
     private static final String SCHEME_HTTP = "HTTP";
 
@@ -56,24 +56,43 @@ public class SpringBootHealthCheckEnricher extends AbstractHealthCheckEnricher {
         return discoverSpringBootHealthCheck(180);
     }
 
-    private Probe discoverSpringBootHealthCheck(int initialDelay) {
+    protected Probe discoverSpringBootHealthCheck(int initialDelay) {
         try {
             if (MavenUtil.hasAllClasses(this.getProject(), REQUIRED_CLASSES)) {
                 Properties properties = SpringBootUtil.getSpringBootApplicationProperties(this.getProject());
-                Integer port = PropertiesHelper.getInteger(properties, SpringBootProperties.MANAGEMENT_PORT,
-                                                           PropertiesHelper.getInteger(properties, SpringBootProperties.SERVER_PORT, DEFAULT_MANAGEMENT_PORT));
-                String scheme = Strings.isNotBlank(properties.getProperty(SpringBootProperties.SERVER_KEYSTORE)) ? SCHEME_HTTPS : SCHEME_HTTP;
-                String contextPath = properties.getProperty(SpringBootProperties.CONTEXT_PATH, "");
-                		
-                // lets default to adding a spring boot actuator health check
-                return new ProbeBuilder().
-                        withNewHttpGet().withNewPort(port).withPath(contextPath + "/health").withScheme(scheme).endHttpGet().
-                        withInitialDelaySeconds(initialDelay).build();
+                return buildProbe(properties, initialDelay);
             }
         } catch (Exception ex) {
             log.error("Error while reading the spring-boot configuration", ex);
         }
         return null;
+    }
+
+    protected Probe buildProbe(Properties springBootProperties, int initialDelay) {
+        Integer managementPort = PropertiesHelper.getInteger(springBootProperties, SpringBootProperties.MANAGEMENT_PORT);
+        boolean usingManagementPort = managementPort != null;
+
+        Integer port = managementPort;
+        if (port == null) {
+            port = PropertiesHelper.getInteger(springBootProperties, SpringBootProperties.SERVER_PORT, DEFAULT_SERVER_PORT);
+        }
+
+        String scheme;
+        String prefix;
+        if (usingManagementPort) {
+            scheme = Strings.isNotBlank(springBootProperties.getProperty(SpringBootProperties.MANAGEMENT_KEYSTORE)) ? SCHEME_HTTPS : SCHEME_HTTP;
+            prefix = springBootProperties.getProperty(SpringBootProperties.MANAGEMENT_CONTEXT_PATH, "");
+        } else {
+            scheme = Strings.isNotBlank(springBootProperties.getProperty(SpringBootProperties.SERVER_KEYSTORE)) ? SCHEME_HTTPS : SCHEME_HTTP;
+            prefix = springBootProperties.getProperty(SpringBootProperties.SERVER_CONTEXT_PATH, "");
+            prefix += springBootProperties.getProperty(SpringBootProperties.SERVLET_PATH, "");
+            prefix += springBootProperties.getProperty(SpringBootProperties.MANAGEMENT_CONTEXT_PATH, "");
+        }
+
+        // lets default to adding a spring boot actuator health check
+        return new ProbeBuilder().
+                withNewHttpGet().withNewPort(port).withPath(prefix + "/health").withScheme(scheme).endHttpGet().
+                withInitialDelaySeconds(initialDelay).build();
     }
 
 }
