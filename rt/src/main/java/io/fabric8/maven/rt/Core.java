bfabea497091a2aa3e975c70846e36be2bec0a57
Rohan Kumar <rohaan@redhat.com>
2017-11-29 01:42:17 +0530

    Added Red Hat License Headers to the test files

--- rt/src/main/java/io/fabric8/maven/rt/Core.java
+++ rt/src/main/java/io/fabric8/maven/rt/Core.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package io.fabric8.maven.rt;
 
 import io.fabric8.kubernetes.client.ConfigBuilder;
@@ -21,8 +37,7 @@ import java.io.*;
 import java.util.List;
 import java.util.Map;
 
-public class Core
-{
+public class Core {
 
     private final String fabric8PluginGroupId = "io.fabric8";
 
@@ -49,12 +64,12 @@ public class Core
         Model model = readPomModelFromFile(pomFile);
 
         Map<String, Plugin> aStringToPluginMap = model.getBuild().getPluginsAsMap();
-        List<Profile> profiles =  model.getProfiles();
-        if(aStringToPluginMap.get(fabric8PluginGroupId + ":" + fabric8PluginArtifactId) != null) {
+        List<Profile> profiles = model.getProfiles();
+        if (aStringToPluginMap.get(fabric8PluginGroupId + ":" + fabric8PluginArtifactId) != null) {
             aStringToPluginMap.get(fabric8PluginGroupId + ":" + fabric8PluginArtifactId).setVersion(getCurrentProjectModel().getVersion());
         } else {
-            for (Profile profile:profiles) {
-                if(profile.getBuild() != null && profile.getBuild().getPluginsAsMap().get(fabric8PluginGroupId + ":" + fabric8PluginArtifactId) != null) {
+            for (Profile profile : profiles) {
+                if (profile.getBuild() != null && profile.getBuild().getPluginsAsMap().get(fabric8PluginGroupId + ":" + fabric8PluginArtifactId) != null) {
                     profile.getBuild().getPluginsAsMap()
                             .get(fabric8PluginGroupId + ":" + fabric8PluginArtifactId)
                             .setVersion(getCurrentProjectModel().getVersion());
