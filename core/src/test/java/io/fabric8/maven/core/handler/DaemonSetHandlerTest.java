117a2bdc74487e372eab544ade95f12721dec3ac
Piyush Garg <piyushgarg001@gmail.com>
2017-12-16 00:17:39 +0530

    Added DaemonHandlerSet Test

---
+++ core/src/test/java/io/fabric8/maven/core/handler/DaemonSetHandlerTest.java
@@ -0,0 +1,218 @@
+package io.fabric8.maven.core.handler;
+
+import io.fabric8.kubernetes.api.model.extensions.Deployment;
+import io.fabric8.maven.core.config.ResourceConfig;
+import io.fabric8.maven.core.config.VolumeConfig;
+import io.fabric8.maven.docker.config.BuildImageConfiguration;
+import io.fabric8.maven.docker.config.ImageConfiguration;
+import io.fabric8.kubernetes.api.model.extensions.*;
+import mockit.Mocked;
+import org.apache.maven.project.MavenProject;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class DaemonSetHandlerTest {
+
+    @Mocked
+    EnvVarHandler envVarHandler;
+
+    @Mocked
+    ProbeHandler probeHandler;
+
+    @Test
+    public void daemonTemplateHandlerTest() {
+
+        MavenProject project = new MavenProject();
+
+        ContainerHandler containerHandler =
+                new ContainerHandler(project, envVarHandler, probeHandler);
+
+        PodTemplateHandler podTemplateHandler = new PodTemplateHandler(containerHandler);
+
+        DaemonSetHandler daemonSetHandler = new DaemonSetHandler(podTemplateHandler);
+
+        List<String> mounts = new ArrayList<>();
+        List<VolumeConfig> volumes1 = new ArrayList<>();
+
+        //volume config with name and multiple mount
+        mounts.add("/path/system");
+        mounts.add("/path/sys");
+
+        VolumeConfig volumeConfig1 = new VolumeConfig.Builder().name("test")
+                .mounts(mounts).type("hostPath").path("/test/path").build();
+        volumes1.clear();
+        volumes1.add(volumeConfig1);
+
+        ResourceConfig config = new ResourceConfig.Builder()
+                .imagePullPolicy("IfNotPresent")
+                .controllerName("testing")
+                .withServiceAccount("test-account")
+                .volumes(volumes1)
+                .build();
+
+        List<String> ports = new ArrayList<>();
+        ports.add("8080");
+        ports.add("9090");
+
+        List<String> tags = new ArrayList<>();
+        tags.add("latest");
+        tags.add("test");
+
+        //container name with alias
+        BuildImageConfiguration buildImageConfiguration = new BuildImageConfiguration.Builder().
+                ports(ports).from("fabric8/maven:latest").cleanup("try")
+                .tags(tags).compression("gzip").build();
+
+        ImageConfiguration imageConfiguration = new ImageConfiguration.Builder().
+                name("test").alias("test-app").buildConfig(buildImageConfiguration)
+                .registry("docker.io").build();
+
+        List<ImageConfiguration> images = new ArrayList<>();
+        images.add(imageConfiguration);
+
+        DaemonSet daemonSet = daemonSetHandler.getDaemonSet(config,images);
+
+        //Assertion
+        assertNotNull(daemonSet.getSpec());
+        assertNotNull(daemonSet.getMetadata());
+        assertNotNull(daemonSet.getSpec().getTemplate());
+        assertEquals("testing",daemonSet.getMetadata().getName());
+        assertEquals("test-account",daemonSet.getSpec().getTemplate()
+                .getSpec().getServiceAccountName());
+        assertFalse(daemonSet.getSpec().getTemplate().getSpec().getVolumes().isEmpty());
+        assertEquals("test",daemonSet.getSpec().getTemplate().getSpec().
+                getVolumes().get(0).getName());
+        assertEquals("/test/path",daemonSet.getSpec().getTemplate()
+                .getSpec().getVolumes().get(0).getHostPath().getPath());
+        assertNotNull(daemonSet.getSpec().getTemplate().getSpec().getContainers());
+
+    }
+
+    @Test
+    //invalid controller name
+    public void daemonTemplateHandlerSecondTest() {
+        try {
+            MavenProject project = new MavenProject();
+
+            ContainerHandler containerHandler =
+                    new ContainerHandler(project, envVarHandler, probeHandler);
+
+            PodTemplateHandler podTemplateHandler = new PodTemplateHandler(containerHandler);
+
+            DaemonSetHandler daemonSetHandler = new DaemonSetHandler(podTemplateHandler);
+
+            List<String> mounts = new ArrayList<>();
+            List<VolumeConfig> volumes1 = new ArrayList<>();
+
+            //volume config with name and multiple mount
+            mounts.add("/path/system");
+            mounts.add("/path/sys");
+
+            VolumeConfig volumeConfig1 = new VolumeConfig.Builder()
+                    .name("test").mounts(mounts).type("hostPath").path("/test/path").build();
+            volumes1.clear();
+            volumes1.add(volumeConfig1);
+
+            //with invalid controller name
+            ResourceConfig config = new ResourceConfig.Builder()
+                    .imagePullPolicy("IfNotPresent")
+                    .controllerName("TesTing")
+                    .withServiceAccount("test-account")
+                    .volumes(volumes1)
+                    .build();
+
+            List<String> ports = new ArrayList<>();
+            ports.add("8080");
+            ports.add("9090");
+
+            List<String> tags = new ArrayList<>();
+            tags.add("latest");
+            tags.add("test");
+
+            //container name with alias
+            BuildImageConfiguration buildImageConfiguration = new BuildImageConfiguration.Builder().
+                    ports(ports).from("fabric8/maven:latest").cleanup("try")
+                    .tags(tags).compression("gzip").build();
+
+            ImageConfiguration imageConfiguration = new ImageConfiguration.Builder().
+                    name("test").alias("test-app").buildConfig(buildImageConfiguration)
+                    .registry("docker.io").build();
+
+            List<ImageConfiguration> images = new ArrayList<>();
+            images.add(imageConfiguration);
+
+            daemonSetHandler.getDaemonSet(config, images);
+        }
+        //asserting the exact message because
+        // it throws the same exception in case controller name is null
+        catch(IllegalArgumentException exception) {
+            assertEquals("Invalid upper case letter 'T' at index 0 for " +
+                    "controller name value: TesTing", exception.getMessage());
+        }
+    }
+
+    @Test
+    //without controller name
+    public void daemonTemplateHandlerThirdTest() {
+        try {
+            MavenProject project = new MavenProject();
+
+            ContainerHandler containerHandler = new
+                    ContainerHandler(project, envVarHandler, probeHandler);
+
+            PodTemplateHandler podTemplateHandler = new PodTemplateHandler(containerHandler);
+
+            DaemonSetHandler daemonSetHandler = new DaemonSetHandler(podTemplateHandler);
+
+            List<String> mounts = new ArrayList<>();
+            List<VolumeConfig> volumes1 = new ArrayList<>();
+
+            //volume config with name and multiple mount
+            mounts.add("/path/system");
+            mounts.add("/path/sys");
+
+            VolumeConfig volumeConfig1 = new VolumeConfig.Builder()
+                    .name("test").mounts(mounts).type("hostPath").path("/test/path").build();
+            volumes1.clear();
+            volumes1.add(volumeConfig1);
+
+            //without controller name
+            ResourceConfig config = new ResourceConfig.Builder()
+                    .imagePullPolicy("IfNotPresent")
+                    .withServiceAccount("test-account")
+                    .volumes(volumes1)
+                    .build();
+
+            List<String> ports = new ArrayList<>();
+            ports.add("8080");
+            ports.add("9090");
+
+            List<String> tags = new ArrayList<>();
+            tags.add("latest");
+            tags.add("test");
+
+            //container name with alias
+            BuildImageConfiguration buildImageConfiguration = new BuildImageConfiguration.Builder().
+                    ports(ports).from("fabric8/maven:latest").cleanup("try").tags(tags)
+                    .compression("gzip").build();
+
+            ImageConfiguration imageConfiguration = new ImageConfiguration.Builder().
+                    name("test").alias("test-app").buildConfig(buildImageConfiguration)
+                    .registry("docker.io").build();
+
+            List<ImageConfiguration> images = new ArrayList<>();
+            images.add(imageConfiguration);
+
+            daemonSetHandler.getDaemonSet(config, images);
+        }
+        //asserting the exact message because
+        //it throws the same exception in case controller name is invalid
+        catch(IllegalArgumentException exception) {
+            assertEquals("No controller name is specified!", exception.getMessage());
+        }
+    }
+}
