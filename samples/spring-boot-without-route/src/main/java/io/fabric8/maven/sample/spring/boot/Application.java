5053ed5ae3d6d7883ca260f72b65711d021a11f8
Piyush Garg <piyushgarg001@gmail.com>
2018-03-01 19:04:28 +0530

    Added sample for spring boot without route This will show how to do configuration in pom.xml for same

---
+++ samples/spring-boot-without-route/src/main/java/io/fabric8/maven/sample/spring/boot/Application.java
@@ -0,0 +1,12 @@
+package io.fabric8.maven.sample.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
