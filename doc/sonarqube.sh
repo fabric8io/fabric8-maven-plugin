e812cb9da2c3ee1b35fc72ccd3c76dac7e50fb66
Roland Huß <roland@ro14nd.de>
2018-03-26 11:27:35 +0200

    Change to new sonarcloud URI

--- doc/sonarqube.sh
+++ doc/sonarqube.sh
@@ -3,6 +3,6 @@
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   # Run SonarQube analysis
   mvn -Pjacoco package sonar:sonar \
-      -Dsonar.host.url=https://sonarqube.com \
+      -Dsonar.host.url=https://sonarcloud.io \
       -Dsonar.login=${SONARQUBE_TOKEN}
 fi
