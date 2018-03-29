fede5eb6fe21eb11f8d2bc16852ec73e654ffab2
Giuseppe Iannello <gi@hubrick.com>
2018-03-29 13:19:40 +0200

    Add LocalStart/LocalStop MOJOs

---
+++ plugin/src/main/java/io/fabric8/maven/plugin/mojo/build/LocalStopMojo.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2016 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package io.fabric8.maven.plugin.mojo.build;
+
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * Stops the docker images configured for this project using d-m-p.
+ *
+ * @author giannello
+ * @since 29/03/18
+ */
+@Mojo(name = "local-stop", defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST)
+public class LocalStopMojo extends io.fabric8.maven.docker.StopMojo {
+}
