cd158d0e96b31f6791190f0a8ee78146e416edb4
Piyush Garg <piyushgarg001@gmail.com>
2018-04-08 19:48:43 +0530

    Change the timestamp tag to latest in generating resource and building images This will fix the different invocations broken like fabric8:build fabric8:resource and hitting invocations separately like fabric8:resource and then fabric8:build This will also fix the problem of generating excessive docker images #1093

--- generator/api/src/test/java/io/fabric8/maven/generator/api/support/BaseGeneratorTest.java
+++ generator/api/src/test/java/io/fabric8/maven/generator/api/support/BaseGeneratorTest.java
@@ -249,7 +249,7 @@ public class BaseGeneratorTest {
 
         setupNameContext(null, null);
         generator = createGenerator(null);
-        assertEquals("%g/%a:%t", generator.getImageName());
+        assertEquals("%g/%a:%l", generator.getImageName());
 
     }
 
