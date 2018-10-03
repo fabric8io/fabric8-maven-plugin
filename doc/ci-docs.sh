9009bcc4f2f0154c10cd1e274d990e314aa1af54
Roland Huß <roland@ro14nd.de>
2018-10-03 10:50:20 +0200

    chore: fix doc generation

--- doc/ci-docs.sh
+++ doc/ci-docs.sh
@@ -14,18 +14,21 @@
 # permissions and limitations under the License.
 #
 
+set -e
+
 echo ============================================
 echo Deploying fabric8-maven-plugin documentation
 echo ============================================
 
-cd doc && \
-mvn -Phtml,pdf package && \
-git clone -b gh-pages git@github.com/fabric8io/fabric8-maven-plugin.git gh-pages && \
-cp -rv target/generated-docs/* gh-pages/ && \
-cd gh-pages && \
-mv index.pdf fabric8-maven-plugin.pdf && \
-git add --ignore-errors * && \
-git commit -m "generated documentation" && \
-git push origin gh-pages && \
-cd .. && \
+mvn install -DskipTests=true
+cd doc
+mvn -Phtml,pdf package
+git clone -b gh-pages git@github.com/fabric8io/fabric8-maven-plugin.git gh-pages
+cp -rv target/generated-docs/* gh-pages/
+cd gh-pages
+mv index.pdf fabric8-maven-plugin.pdf
+git add --ignore-errors *
+git commit -m "generated documentation"
+git push origin gh-pages
+cd ..
 rm -rf gh-pages target
