0c3c3fcefd97528c2e530f69413f857f8c10d407
Kurt T Stam <kstam@redhat.com>
2018-10-05 10:50:48 +0200

    3.5.x - [fix] Apply updates to the new discovery.3scale.net labels/annotations (#1405)
    * feat: Adding Service Annotations for Service Discovery for Camel Rest DSL Projects -  https://issues.jboss.org/browse/ENTESB-9252

--- enricher/fabric8/src/main/java/io/fabric8/maven/enricher/fabric8/ServiceDiscoveryEnricher.java
+++ enricher/fabric8/src/main/java/io/fabric8/maven/enricher/fabric8/ServiceDiscoveryEnricher.java
@@ -17,6 +17,7 @@ package io.fabric8.maven.enricher.fabric8;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -27,27 +28,31 @@ import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+import io.fabric8.kubernetes.api.builder.TypedVisitor;
+import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import io.fabric8.kubernetes.api.model.ServicePort;
 import io.fabric8.maven.core.util.Configs;
-import io.fabric8.maven.core.util.MapUtil;
 import io.fabric8.maven.enricher.api.BaseEnricher;
 import io.fabric8.maven.enricher.api.EnricherContext;
-import io.fabric8.maven.enricher.api.Kind;
 
 public class ServiceDiscoveryEnricher extends BaseEnricher {
     static final String ENRICHER_NAME        = "f8-service-discovery";
 
     //Default Prefix
-    static final String PREFIX    = "discovery.3scale.net/";
+    static final String PREFIX    = "discovery.3scale.net";
     //Service Annotations
     static final String DISCOVERY_VERSION = "discovery-version";
     static final String SCHEME            = "scheme";
     static final String PATH              = "path";
+    static final String PORT              = "port";
     static final String DESCRIPTION_PATH  = "description-path";
     //Service Labels
     static final String DISCOVERABLE      = "discoverable";
 
     private File springConfigDir;
     private String path             = null;
+    private String port             = "80";
     private String scheme           = "http";
     private String descriptionPath  = null;
     private String discoverable     = null;
@@ -58,6 +63,7 @@ public class ServiceDiscoveryEnricher extends BaseEnricher {
         discoverable,
         discoveryVersion,
         path,
+        port,
         scheme,
         springDir;
 
@@ -73,56 +79,68 @@ public class ServiceDiscoveryEnricher extends BaseEnricher {
     }
 
     @Override
-    public Map<String, String> getAnnotations(Kind kind) {
+    public void addMissingResources(final KubernetesListBuilder listBuilder) {
+        listBuilder.accept(new TypedVisitor<ServiceBuilder>() {
 
-        if (kind == Kind.SERVICE) {
-
-            tryCamelDSLProject();
-
-            if (discoverable != null) {
-                Map<String, String> annotations = new HashMap<>();
-                annotations.put(PREFIX + DISCOVERY_VERSION, getConfig(Config.discoveryVersion, discoveryVersion));
-                annotations.put(PREFIX + SCHEME           , getConfig(Config.scheme, scheme));
-                if (getConfig(Config.path, path) != null) {
-                    annotations.put(PREFIX + PATH             , getConfig(Config.path, path));
-                }
-                if (getConfig(Config.descriptionPath, descriptionPath) != null) {
-                    annotations.put(PREFIX + DESCRIPTION_PATH , getConfig(Config.descriptionPath, descriptionPath));
-                }
-                if (log.isVerboseEnabled()) {
-                    for (String annotationName : annotations.keySet()) {
-                        log.verbose("Add %s annotation: %s=%s", PREFIX, 
-                                annotationName, annotations.get(annotationName));
-                    }
-                }
-                return annotations;
+            @Override
+            public void visit(ServiceBuilder serviceBuilder) {
+                addAnnotations(serviceBuilder);
             }
-        }
 
-        return super.getAnnotations(kind);
+        });
+        
+        
     }
 
-    @Override
-    public Map<String, String> getLabels(Kind kind) {
+    protected void addAnnotations(ServiceBuilder serviceBuilder) {
 
-        if (kind == Kind.SERVICE) {
-
-            tryCamelDSLProject();
-
-            if (discoverable != null) {
-                Map<String, String> labels = new HashMap<>();
-                String labelName = PREFIX + DISCOVERABLE;
-                labels.put(labelName, getConfig(Config.discoverable, discoverable));
-                if (log.isVerboseEnabled()) {
-                    log.verbose("Add %s label: %s=%s", PREFIX, 
-                            labelName, labels.get(labelName));
-                }
-                return labels;
+        if (serviceBuilder.buildSpec() != null) {
+            List<ServicePort> ports = serviceBuilder.buildSpec().getPorts();
+            if (! ports.isEmpty()) {
+                ServicePort firstServicePort = ports.iterator().next();
+                port = firstServicePort.getPort().toString();
+                log.info("Using first mentioned service port '%s' " , port);
+            } else {
+                log.warn("No service port was found");
             }
         }
-        return super.getLabels(kind);
-    }
+        
+        tryCamelDSLProject();
+        
+        if (discoverable != null) {
 
+            String labelName = PREFIX;
+            String labelValue = getConfig(Config.discoverable, discoverable);
+            serviceBuilder.editOrNewMetadata().addToLabels(
+                    labelName, labelValue).and().buildMetadata();
+            log.info("Add %s label: \"%s\" : \"%s\"", PREFIX, 
+                    labelName, labelValue);
+
+            Map<String, String> annotations = new HashMap<>();
+            annotations.put(PREFIX + "/" + DISCOVERY_VERSION, getConfig(Config.discoveryVersion, discoveryVersion));
+            annotations.put(PREFIX + "/" + SCHEME           , getConfig(Config.scheme, scheme));
+            String resolvedPath = getConfig(Config.path, path);
+            if (resolvedPath != null) {
+                if (!resolvedPath.startsWith("/")) {
+                    resolvedPath = "/" + resolvedPath;
+                }
+                annotations.put(PREFIX + "/" + PATH          , resolvedPath);
+            }
+            annotations.put(PREFIX + "/" + PORT              , getConfig(Config.port, port));
+            String resolvedDescriptionPath = getConfig(Config.descriptionPath, descriptionPath);
+            if (resolvedDescriptionPath != null) {
+                if (!resolvedDescriptionPath.toLowerCase().startsWith("http") && !resolvedDescriptionPath.startsWith("/")) {
+                    resolvedDescriptionPath = "/" + resolvedDescriptionPath;
+                }
+                annotations.put(PREFIX + "/" + DESCRIPTION_PATH , resolvedDescriptionPath);
+            }
+            for (String annotationName : annotations.keySet()) {
+                log.info("Add %s annotation: \"%s\" : \"%s\"", PREFIX, 
+                        annotationName, annotations.get(annotationName));
+            }
+            serviceBuilder.editMetadata().addToAnnotations(annotations).and().buildMetadata();
+        }
+    }
     public void tryCamelDSLProject(){
         File camelContextXmlFile = new File(springConfigDir.getAbsoluteFile() + "/camel-context.xml");
         if (camelContextXmlFile.exists()) {
@@ -138,12 +156,15 @@ public class ServiceDiscoveryEnricher extends BaseEnricher {
                     discoverable = "true";
                     if (nl.getAttributes().getNamedItem("scheme")!=null) {
                         scheme = nl.getAttributes().getNamedItem("scheme").getNodeValue();
+                        log.verbose("Obtained scheme '%s' from camel-context.xml " , scheme);
                     }
                     if (nl.getAttributes().getNamedItem("contextPath")!=null) {
                         path = nl.getAttributes().getNamedItem("contextPath").getNodeValue();
+                        log.verbose("Obtained path '%s' from camel-context.xml " , path);
                     }
                     if (nl.getAttributes().getNamedItem("apiContextPath")!=null) {
                         descriptionPath = nl.getAttributes().getNamedItem("apiContextPath").getNodeValue();
+                        log.verbose("Obtained descriptionPath '%s' from camel-context.xml " , descriptionPath);
                     }
                 }
             } catch (Exception e) {
