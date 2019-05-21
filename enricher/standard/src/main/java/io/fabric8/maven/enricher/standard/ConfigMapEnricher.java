c7caf812ec3c36caa4496316ac512832ba8b04a3
Paulo Henrique Alves <paulohenriqueas13@gmail.com>
2019-05-21 08:16:33 -0300

    Trim name param.

--- enricher/standard/src/main/java/io/fabric8/maven/enricher/standard/ConfigMapEnricher.java
+++ enricher/standard/src/main/java/io/fabric8/maven/enricher/standard/ConfigMapEnricher.java
@@ -92,7 +92,7 @@ public class ConfigMapEnricher extends BaseEnricher {
         final Map<String, String> configMapFromConfiguration;
         try {
             configMapFromConfiguration = createConfigMapFromConfiguration(configMap);
-            String configMapName = (configMap == null || configMap.getName() == null || configMap.getName().trim().isEmpty()) ? "xmlconfig" : configMap.getName();
+            String configMapName = (configMap == null || configMap.getName() == null || configMap.getName().trim().isEmpty()) ? "xmlconfig" : configMap.getName().trim();
             log.debug("configMapName :: ".concat(configMapName));
 
             if(!configMapFromConfiguration.isEmpty() && !checkIfItemExists(builder, configMapName)) {
