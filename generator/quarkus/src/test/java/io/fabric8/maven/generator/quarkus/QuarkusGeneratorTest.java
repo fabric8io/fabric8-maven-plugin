9a68bfebb980c3dde8a2505651bb7b48e3779746
Juan Zuriaga <juazuri@gmail.com>
2019-10-27 11:04:35 +0100

    Add changelog information and format license header.

--- generator/quarkus/src/test/java/io/fabric8/maven/generator/quarkus/QuarkusGeneratorTest.java
+++ generator/quarkus/src/test/java/io/fabric8/maven/generator/quarkus/QuarkusGeneratorTest.java
@@ -3,14 +3,14 @@
  *
  * Red Hat licenses this file to you under the Apache License, version
  * 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied. See the License for the specific language governing
+ * implied.  See the License for the specific language governing
  * permissions and limitations under the License.
  */
 package io.fabric8.maven.generator.quarkus;
@@ -59,7 +59,7 @@ public class QuarkusGeneratorTest {
 
     @Mocked
     private ProcessorConfig config;
-    
+
     private Properties projectProps = new Properties();
 
     @Before
@@ -107,7 +107,7 @@ public class QuarkusGeneratorTest {
             config.getConfig("quarkus", "from"); result = BASE_JAVA_IMAGE;
         }};
         // @formatter:on
-        
+
         QuarkusGenerator generator = new QuarkusGenerator(ctx);
         List<ImageConfiguration> resultImages = null;
         List<ImageConfiguration> existingImages = new ArrayList<>();
